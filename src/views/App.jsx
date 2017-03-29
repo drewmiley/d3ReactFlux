768f462cde53e37ca3caa6bfb960dbd5c243d518
Drew Miley <drewmiley92@gmail.com>
2017-03-29 18:50:22 +0100

    Moves towards React-Flux file structure

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import data from '../data/Store';
+
 import BarChart from './components/BarChart';
 
 class App extends Component {
 	render() {
 		return <div>
-			<BarChart data='Hello World' />
+			<BarChart data={data} />
 		</div>
 	};
 };
