0246c606d2a74bc55c4c1942a71902ffb719767f
Drew Miley <drew.miley@opencastsoftware.com>
2017-04-05 16:55:53 +0100

    Import now working on unit tests

--- test/data/RecordsStore_spec.js
+++ test/data/RecordsStore_spec.js
@@ -1,6 +1,15 @@
+import { ReduceStore } from 'flux/utils';
+import Immutable from 'immutable';
+
+import RecordsActionTypes from '../../src/data/RecordsActionTypes';
+import RecordsDispatcher from '../../src/data/RecordsDispatcher';
+import RecordsStore from '../../src/data/RecordsStore';
+
 describe('RecordsStore', () => {
 
 	it('should fail', () => {
+		let recordsStore = RecordsStore;
+		console.log(recordsStore.getInitialState().get('title'));
 		expect(true).toBe(false);
 	});
 
